e9bb113d2dd7119bd65736be4d4514c1ac2d6984
Janos Cserna <JanosCserna@gmail.com>
2022-11-15 17:19:47 -0800

    initial commit with submodule

---
+++ source/bin/api.d
@@ -0,0 +1,1 @@
+bin/api.o: ../common_code/api.c ../common_code/api.h
---
+++ source/bin/my-project.d
@@ -0,0 +1,9 @@
+bin/my-project.o: my-project.c ../common_code/api.h \
+ ../common_code/api-asm.h \
+ /Users/janoscserna/Documents/code/compiler/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /Users/janoscserna/Documents/code/compiler/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /Users/janoscserna/Documents/code/compiler/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /Users/janoscserna/Documents/code/compiler/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /Users/janoscserna/Documents/code/compiler/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /Users/janoscserna/Documents/code/compiler/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /Users/janoscserna/Documents/code/compiler/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h
